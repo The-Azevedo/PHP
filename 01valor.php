cf360fe1b6dc673b260c9790770ce57a1e228f62
Wender Azevedo <56097155+The-Azevedo@users.noreply.github.com>
2021-07-19 14:11:44 -0300

    da

---
+++ 01valor.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Valor calculado</title>
+</head>
+<body>
+    <div>
+        <?php
+        $valor = $_GET["v"];
+        echo "O valor enviado foi $valor";
+        ?>
+    </div>
+</body>
+</html>
--- teste.php
+++ teste.php
@@ -1,17 +1,18 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
-    <h1>Testando</h1>
-<?php
-echo "Olá Mundo";
+<div>
+    <form action="get" action="01valor.php">
+        valor: <input type="number" name="v"/>
+        <input type="submit" value="Calcular Raiz"/>
+    </form>
 
-?>
+</div>
     
 </body>
 </html>
